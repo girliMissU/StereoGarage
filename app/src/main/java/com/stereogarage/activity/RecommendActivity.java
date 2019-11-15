9bd50e819adc37aeaa685a688376f43767aeb8ad
626929685@qq.com <515680@Hxy!>
2019-11-15 14:40:16 +0800

    tuijian

--- app/src/main/java/com/stereogarage/activity/RecommendActivity.java
+++ app/src/main/java/com/stereogarage/activity/RecommendActivity.java
@@ -15,7 +15,7 @@ public class RecommendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommend);
+        setContentView(R.layout.activity_record);
     }
 
     @Override
